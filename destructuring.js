4cacef0cf10c59f325e81a2085b509f876d02b0b
Joe Warren <themightyjoew@gmail.com>
2017-10-11 16:01:35 -0500

    first 2

--- destructuring.js
+++ destructuring.js
@@ -13,15 +13,18 @@ var carDetails = {
 
 // CODE HERE
 
+const {color, make, model, year} = carDetails;
 
 // ========================
 
 
-// In the function below named greeting, it is receiving an object as a parameter. Use object destructuring to save the object properties to new variables. The property names are firstName, lastName, and title. Return the concatenated string.
+// In the function below named greeting, it is receiving an object as a parameter. Use object destructuring 
+//to save the object properties to new variables. The property names are firstName, lastName, and title. 
+//Return the concatenated string.
 
 function greeting( obj ) {
   // CODE HERE
-  
+  let {firstName, lastName, title} = obj;
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
 }
 
@@ -29,10 +32,16 @@ function greeting( obj ) {
 // ========================
 
 
-// Write a function called totalPopulation that will take in an object. That object will have 4 properties named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to save the property values to new variables. Sum up the values and return the total number.
+// Write a function called totalPopulation that will take in an object. That object will have 4 properties 
+//named utah, california, texas and arizona. The property values will be numbers. Use object destructuring to 
+//save the property values to new variables. Sum up the values and return the total number.
 
   // CODE HERE
-
+const totalPopulation = (obj) => {
+  let {utah, california, texas, arizona} = obj;
+  let sum = utah + california + texas + arizona;
+  return sum;
+}
 
 // ========================
 
